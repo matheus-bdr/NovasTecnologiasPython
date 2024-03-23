5cf3739f472faf83bd67a32b409b594861bbf817
matheus-bdr <mmatheuscruzs@gmail.com>
2024-03-22 21:02:45 -0300

    commit

---
+++ ExerciciosAula1e2/Exercicio3.py
@@ -0,0 +1,8 @@
+#Escreva um aplicativo que lê um inteiro, determina e imprime se ele é ímpar
+#ou par.
+num1 = int(input("digite um numero: "))
+
+if num1%2 == 0:
+    print("numero par")
+else:
+    print("numero impar")
---
+++ ExerciciosAula1e2/Exercicio4.py
@@ -0,0 +1,8 @@
+"""Escreva um aplicativo que insere um número consistindo em cinco dígitos
+do usuário, separa o número em seus dígitos individuais e imprime os
+dígitos separados uns dos outros por três espaços cada. Por exemplo, se o
+usuário digitar o número 42339, o programa deve imprimir: 4 2 3 3 9."""
+
+n = (input("digite um numero: "))
+ 
+
